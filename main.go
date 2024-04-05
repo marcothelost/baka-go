79d6d17985bb5df59b3b0fb87a4e0daccd165783
marcothelost <marcothelost@proton.me>
2024-04-05 16:30:37 +0200

    Getting arguments and flags from the CLI

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("BakaGo")
+	var args []string
+	var flags []string
+
+	for _, arg := range os.Args[1:] {
+		if len(arg) > 1 && arg[0] == '-' {
+			flags = append(flags, arg[1:])
+			continue
+		}
+		args = append(args, arg)
+	}
+
+	fmt.Println("Arguments:")
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	fmt.Println("Flags:")
+	for _, flag := range flags {
+		fmt.Println(flag)
+	}
 }
